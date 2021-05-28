ae5791277cecb0019cbf4da1dbe3859c1a0215b1
Luca-Riz <luca983@gmail.com>
2021-05-28 15:38:15 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+// genero un numero random da 1 a 6 per giocatore 1
+
+
+
+
+// genero un numero random da 1 a 6 per giocatore 2
+
+
+
+// confronto i due risultati e dò la vittoria al punteggio più alto
