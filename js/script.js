75d225e6ee3e85d26e16101db2ab65f70d8d1249
Luca-Riz <luca983@gmail.com>
2021-05-28 15:55:43 +0200

    add nuove diciture console log - giocabile su log

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@
 // genero un numero random da 1 a 6 per giocatore 1
 
 var dado1 = Math.floor(Math.random() * 6 + 1);
-console.log(dado1);
+console.log('Il tuo dado segna: ' + dado1);
 
 // genero un numero random da 1 a 6 per giocatore 2
 
 var dado2 = Math.floor(Math.random() * 6 + 1);
-console.log(dado2);
+console.log('Il computer ha fatto: ' + dado2);
 
 // confronto i due risultati e dò la vittoria al punteggio più alto
 
 if (dado1 > dado2) {
-    console.log('vince dado1');
+    console.log('Hai vinto!');
 } else if (dado1 < dado2) {
-    console.log('vince dado2');
+    console.log('Hai perso');
 } else {
-    console.log('pareggio');
+    console.log('Pareggio');
 }
