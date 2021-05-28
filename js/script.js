0bdfa2634c77c1f5973cf05333e6077797ab8d0d
Luca-Riz <luca983@gmail.com>
2021-05-28 15:46:14 +0200

    add 2 random dice

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@
 
 // genero un numero random da 1 a 6 per giocatore 1
 
-
-
+var dado1 = Math.floor(Math.random() * 6 + 1);
+console.log(dado1);
 
 // genero un numero random da 1 a 6 per giocatore 2
 
-
+var dado2 = Math.floor(Math.random() * 6 + 1);
+console.log(dado2);
 
 // confronto i due risultati e dò la vittoria al punteggio più alto
